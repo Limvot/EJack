75eb66a8dc0c90d04838d83f37ed08025e2e36e7
Nathan Braswell <miloignis@gmail.com>
2015-10-10 13:01:19 -0400

    finally fixed the bug. Nothing with elixir or anything, though I did change gen_tcp to be active in an attempt to fix it. The real problem is that parse_data couldn't handle two packets in the same data

--- lib/ejack.ex
+++ lib/ejack.ex
@@ -23,15 +23,19 @@ defmodule EJack.TCPIT do
                 
   def listen(port) do
     #{:ok, socket} = :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
-    {:ok, socket} = :gen_tcp.listen(port, [:binary, packet: 0, active: false, reuseaddr: true])
+    #{:ok, socket} = :gen_tcp.listen(port, [:binary, packet: 0, active: false, reuseaddr: true])
+    {:ok, socket} = :gen_tcp.listen(port, [:binary, packet: 0, active: true, reuseaddr: true])
     IO.puts "Accepting connections on port #{port}"
     loop_acceptor(socket)
   end
   defp loop_acceptor(socket) do
     {:ok, client} = :gen_tcp.accept(socket)
-    {:ok, pid} = Task.Supervisor.start_child(EJack.TaskSupervisor, fn -> handshake(client) end)
-    :gen_tcp.controlling_process(client, pid)
-    loop_acceptor(socket)
+    #{:ok, pid} = Task.Supervisor.start_child(EJack.TaskSupervisor, fn -> handshake(client) end)
+    #:gen_tcp.controlling_process(client, pid)
+    #loop_acceptor(socket)
+
+    {:ok, pid} = Task.Supervisor.start_child(EJack.TaskSupervisor, fn -> loop_acceptor(socket) end)
+    handshake(client)
   end
   defp handshake(socket) do
     guid = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
@@ -69,11 +73,27 @@ defmodule EJack.TCPIT do
     # if len > 0?
     mask_key = binary_part(data, 2, 4)
     masked_data = binary_part(data, 6, len)
-    for i <- 0..(len-1),  into: "", do: << binary_index(masked_data,i) ^^^ binary_index(mask_key,rem(i,4)) >>
+    parsed = for i <- 0..(len-1),  into: "", do: << binary_index(masked_data,i) ^^^ binary_index(mask_key,rem(i,4)) >>
+
+    realsize = div(bit_size(data),8)
+    if realsize > 6+len do
+      parsed = parsed <> parse_data(binary_part(data, 6+len, realsize-(6+len)))
+    end
+    parsed
+  end
+
+  defp read_string(string) do
+    {len_str, string} = String.split_at(string, 3)
+    len = String.to_integer(len_str)
+    String.split_at(string, len)
+  end
+
+  defp int_to_string(int, len) do
+    str = Integer.to_string(int) |> String.rjust(3, 48)
   end
 
   defp serve(socket, current_data) do
-    current_data = current_data <> parse_data(read_line(socket))
+    IO.puts("casing with #{div(bit_size(current_data),8)}")
     case current_data do
       "PIN" <> rest ->
         IO.puts("ping!")
@@ -98,23 +118,53 @@ defmodule EJack.TCPIT do
       "005" <> rest ->
         IO.puts("player name")
         {_, current_data} = String.split_at(current_data, 3)
+        {name, current_data} = read_string(current_data)
+        IO.puts(name)
 
       "010" <> rest ->
-        IO.puts("joining host")
+        IO.puts("host something")
         {_, current_data} = String.split_at(current_data, 3)
+        {host_code, current_data} = String.split_at(current_data, 4)
+        if host_code == "0000" do
+          IO.puts("become host")
+          host_code = "DDDA"
+          write_wb_packet(int_to_string(10, 3) <> host_code, socket)
+        else
+          IO.puts("join host #{host_code}")
+          write_wb_packet(int_to_string(1, 3) <> "01", socket)
+        end
 
-      _ -> IO.puts("not enough data/invalid/unknown message")
-        {code, current_data} = String.split_at(current_data, 3)
-        IO.puts("code: #{code}")
+      _ -> 
+        IO.puts("reading some more data, have #{div(bit_size(current_data),8)}")
+        current_data = current_data <> parse_data(read_line(socket))
+        IO.puts("read more data, have #{div(bit_size(current_data),8)}")
+      #_ -> IO.puts("not enough data/invalid/unknown message")
     end
     serve(socket, current_data)
   end
   defp read_line(socket) do
-    {:ok, data} = :gen_tcp.recv(socket, 0)
+    #{:ok, data} = :gen_tcp.recv(socket, 0)
+    #data
+    data = receive do
+      #{:ok, data} -> data
+      {:tcp, port, data} -> data
+    end
+    IO.puts("READ LINE, have #{bit_size(data)} /8 =  #{div(bit_size(data),8)}")
     data
   end
+  defp read_line_low_block(socket) do
+    case :gen_tcp.recv(socket, 0, 100) do
+      {:ok, data} -> data
+      {:error, _} -> ""
+    end
+  end
   defp write_line(line, socket) do
+    IO.puts("sending:#{line}")
     :gen_tcp.send(socket, line)
+  end
+  defp write_wb_packet(line, socket) do
+    IO.puts("sending:#{line}")
+    :gen_tcp.send(socket, <<129, String.length(line)>> <> line)
   end
 end
 
